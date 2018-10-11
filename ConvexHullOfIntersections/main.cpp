1641e3a2d92e2a6271d2826558f2664711b1d0a4
Gabor Fliszar <gabor.fliszar@gmail.com>
2018-10-11 16:24:22 +0200

    test

--- ConvexHullOfIntersections/main.cpp
+++ ConvexHullOfIntersections/main.cpp
@@ -4,10 +4,11 @@
 int main()
 {
 	//Usage: 
-// - if a Solution object is created without any parameters(e.g.: Solution s;) -> the input will be the STD input (cin)
-// - if a Solution object is created with 1 parameter(e.g.:Solution s("input.txt");) -> the input will be the file of given name in parameter
-// - the 2nd and 3rd parameter is created for modultest to check the expected values (see below)
-//Solution s("input.txt", "expected_intersection_points.txt", "expected_convex_hull_points.txt");
+	// - if a Solution object is created without any parameters(e.g.: Solution s;) -> the input will be the STD input (cin)
+	// - if a Solution object is created with 1 parameter(e.g.:Solution s("input.txt");) -> the input will be the file of given name in parameter
+	// - the 2nd and 3rd parameter is created for modultest to check the expected values (see below)
+	//Solution s("input.txt", "expected_intersection_points.txt", "expected_convex_hull_points.txt");
+
 	try
 	{
 		ConvexHull::Solution s("input0.txt");
